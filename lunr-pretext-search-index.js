5c65d1aaa941c1fadb14a84e7987b720c89f19ca
EPaDelByLaws <budboman@gmail.com>
2023-06-02 11:17:37 -0400

    Latest build of target web.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,76 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "section-1",
+  "level": "1",
+  "url": "section-1.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Article I<a href=\"\" data-knowl=\"\" class=\"id-ref fn-knowl original\" data-refid=\"hk-fn-1\" id=\"fn-1\"><sup> 1 <\/sup><\/a>: Name and Purpose",
+  "body": " Article I Revised in 1971, 1977, 1979, 1980, 2008, and 2019 : Name and Purpose   The name of this Section shall be the Eastern Pennsylvania and Delaware Section of the Mathematical Association of America (MAA).  The purposes of the Eastern Pennsylvania and Delaware Section shall be to advance the mission of the MAA on a regional level (namely within the territory defined in Article II below); to offer guidance to the MAA as it forms and fulfills its mission; to provide professional development and networking activities for section members and mathematics students in geographically accessible locations; and to promote discussion and action on issues affecting mathematics teaching, learning, and research in the region.   "
+},
+{
+  "id": "section-2",
+  "level": "1",
+  "url": "section-2.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Article II: Membership",
+  "body": " Article II: Membership  The membership of the Eastern Pennsylvania and Delaware Section shall be members of the Mathematical Association of America whose MAA mailing addresses are in Delaware, ZIP codes from 19700 to 19980, and in that part of Pennsylvania with ZIP codes from 16900 to 19612. Exceptions may be made by the MAA headquarters office upon request of the affected member.  "
+},
+{
+  "id": "section-3",
+  "level": "1",
+  "url": "section-3.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Article III: Avoiding Implicit Bias",
+  "body": " Article III: Avoiding Implicit Bias  The EPaDel Section of the Mathematical Association of America shall make every effort to avoid implicit bias and to reflect the diversity of the section. The Section shall ensure its committee members, officers, invited speakers, awardees, and other leaders represent the various geographic regions, types of institutions, and diverse membership within the section. It shall strive for balanced representation with regard to gender as well as underrepresented groups, and shall seek out nominations for any positions or awards to help achieve that balance. Section committees and members may consult the most recent version of the document Guidelines for MAA Selection Committees: Avoiding Implicit Bias for guidance.   "
+},
+{
+  "id": "section-4",
+  "level": "1",
+  "url": "section-4.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Article IV: Officers and Executive Committee",
+  "body": " Article IV: Officers and Executive Committee    The officers of the Section shall be the Chair, the Vice Chair, the Secretary, and the Treasurer.    The Executive Committee of the Section shall consist of the officers, six members-at-large, the Section Representative to the MAA Congress (ex officio), and non-XXvoting members as stated in Article IV, Section 7.    Members of the Executive Committee shall be members of the Mathematical Association of America and of the Section.    Members of the Executive Committee who are elected by the Section shall be elected at the Annual Fall Meeting.    The Chair and Vice Chair shall be elected during the fall of odd numbered years and each will serve a two-year term. The Chair and Vice Chair may not serve in the same position for consecutive terms.    The Secretary and Treasurer shall be elected for staggered three-year terms; the Treasurer to be elected when the four digit year is congruent to zero mod 3; the Secretary when the four digit year is congruent to 1 mod 3.    Members-at-large shall be elected for staggered three-year terms with two members elected each year. A member-at-large shall serve at most two consecutive terms.      The Nominating Committee shall prepare a slate of candidates willing to stand for election at the Annual Fall Meeting. Further nominations by Section members shall be accepted at the Annual Fall Meeting provided that the candidates have agreed to serve. In the case of a tie, the Nominating Committee shall make the selection from among those tied. The Nominating Committee shall consist of at least three members and shall be appointed by the Chair at least six weeks before the Annual Fall Meeting.    The duties of members of the Executive Committee shall be as follows.    The Chair shall preside at all meetings of the Section and of the Executive Committee. The Chair shall have general charge of the Section. The Chair, in consultation with the Executive Committee, shall appoint all committees and the committee chairs except the Program Committee, which is appointed by the Vice Chair.    The Vice Chair shall assume the duties of the Chair in the absence of the Chair and shall assist the Chair with the business of the Section. The Vice Chair shall appoint and chair the Program Committee for each Section meeting.    The Secretary shall record minutes of Executive Committee meetings, act as communication liaison between the Section and the MAA headquarters office, manage the work of the Crawford Teaching Award committee and the submission of Section nominations for the MAA Haimo Award, and complete all reports on section activities required by the association.    The Treasurer shall administer the financial affairs of the Section under the direction of the Executive Committee. The Treasurer shall collect registration fees and voluntary dues, receive the funds provided by MAA, arrange for safekeeping of all Section monies, maintain proper and accurate books of account of such monies, pay all Section bills in a timely manner, and file the annual financial report of the Section with the MAA headquarters office.    Members-at-large of the Executive Committee shall assist the Vice Chair in planning the Section’s programs and perform other tasks as requested by the Chair.     The Chair, in consultation with the Executive Committee, may appoint non-voting members to the Executive Committee to handle special tasks, such as the Webmaster, Section NExT Coordinator, Student Activities Coordinator, etc. Other positions can be created as needed by the Executive Committee. Such appointments shall normally be for one to three years, and they may be renewed.  The quorum for Executive Committee meetings is five voting members. The Executive Committee shall conduct the affairs of the Section between meetings. It is empowered to fill any vacancy on the Executive Committee (except that of MAA Congress Representative) until the next section business meeting.   "
+},
+{
+  "id": "section-5",
+  "level": "1",
+  "url": "section-5.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Article V: Meetings",
+  "body": " Article V: Meetings    The Section shall hold two regular meetings each academic year, one in the fall and one in the spring. A business meeting shall be held in conjunction with each meeting. Elections shall be held during the Fall Meeting. Newly elected members of the Executive Committee shall assume office upon the adjournment of the Fall Meeting.    The time and place of meetings shall be determined by the Executive Committee.    Upon written request of at least twenty Section members, and within ninety days of receipt, the Executive Committee will call and arrange a special meeting of the section.    The date and time of any regular or special business meeting will be publicly posted at least twenty days in advance of the meeting.    A quorum shall consist of not fewer than fifteen members of the Section, and no business may be transacted at meetings where less than a quorum is present.    "
+},
+{
+  "id": "section-6",
+  "level": "1",
+  "url": "section-6.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Article VI: Fees and Use of Assets",
+  "body": " Article VI: Fees and Use of Assets    A registration fee may be collected from those who attend any regular meeting of the Section. The Executive Committee shall set the meeting fees.    The Executive Committee may request that members make donations to special funds, such as those supporting awards of the sections.    The Executive Committee shall annually appoint a Committee to review the financial records of the section provided by the Section Treasurer.    The assets of the Eastern Pennsylvania and Delaware Section shall be used exclusively to further the purposes of the Section. In the event of the dissolution of the Section, the remaining assets shall be returned to the Mathematical Association of America to be used for a purpose consistent with the mission of that organization.    "
+},
+{
+  "id": "section-7",
+  "level": "1",
+  "url": "section-7.html",
+  "type": "Section",
+  "number": "7",
+  "title": "Article VII: Amendments",
+  "body": " Article VII: Amendments    These bylaws may be amended by two-thirds of the votes cast by a quorum at any business meeting of the Section, subject to subsequent approval by the Board of Directors of the Mathematical Association of America.    Any member or group of members of the Section can propose an amendment to these bylaws. All proposed amendments shall be submitted to the Secretary who will forward them to the Executive Committee. The Executive Committee will review each proposal and vote to approve or disapprove. If approved by the Executive Committee the proposed amendment shall be publicly posted for review by the Section membership at least twenty days prior to being voted on by the Section membership. The vote will take place at the next regular business meeting after the twenty day review period has elapsed.    A complete revision of these bylaws shall be subject to all of the same procedures required for other amendments to these bylaws.    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
